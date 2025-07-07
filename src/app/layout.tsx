898a79a1b21b3cd3a999624f656973d89ebdaf50
오승준 <skku03062@g.skku.edu>
2025-07-07 16:13:57 +0900

    vercel analytics 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ProtectedRoute from "@/components/auth/ProtectedRoute";
 import QueryProvider from "@/components/providers/QueryProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <QueryProvider>
           <ProtectedRoute>{children}</ProtectedRoute>
         </QueryProvider>
+        <Analytics />
       </body>
     </html>
   );
